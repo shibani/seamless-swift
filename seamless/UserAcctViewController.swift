4808cd9e67ee71e273a4a9b3501f8091badad1dd
Shibani <shibani@kindreddevelopment.com>
2016-08-03 23:41:26 -0400

    Adding Stripe - getting token back

--- seamless/UserAcctViewController.swift
+++ seamless/UserAcctViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class UserAcctViewController: UIViewController, UITextFieldDelegate {
     
-    
     @IBOutlet weak var ccNum: UITextField!
     
     @IBOutlet weak var ccExp: UITextField!
@@ -18,6 +17,9 @@ class UserAcctViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var ccCVV: UITextField!
     
     @IBOutlet weak var ccName: UITextField!
+    
+    @IBOutlet weak var saveMyInfo: DLRadioButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +34,7 @@ class UserAcctViewController: UIViewController, UITextFieldDelegate {
         ccNum.autocapitalizationType = UITextAutocapitalizationType.None
         ccNum.delegate = self
         
-        ccExp.placeholder = "MM-DD-YYYY"
+        ccExp.placeholder = "MM/YYYY"
         ccExp.textColor = UIColor.lightGrayColor()
         ccExp.font = UIFont.italicSystemFontOfSize(13)
         ccExp.autocorrectionType = UITextAutocorrectionType.No
@@ -87,6 +89,78 @@ class UserAcctViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func saveBtnClicked(sender: AnyObject) {
         print("save btn clicked!")
+        
+        // Initiate the card
+        let cardParams = STPCardParams()
+        
+        // Split the expiration date to extract Month & Year
+        if self.ccExp.text!.isEmpty == false {
+            let expDate = self.ccExp.text!.componentsSeparatedByString("/")
+            let expMonth = UInt(expDate[0])
+            let expYear = UInt(expDate[1])
+            //let expMonth = Int(UInt(expDate[0])!)
+            //let expYear = Int(UInt(expDate[1])!)
+            
+            // Send the card info to Strip to get the token
+            cardParams.number = self.ccNum.text
+            cardParams.cvc = self.ccCVV.text
+            cardParams.expMonth = expMonth!
+            cardParams.expYear = expYear!
+        }
+        
+        STPAPIClient.sharedClient().createTokenWithCard(cardParams) { (token, error) in
+            if let error = error {
+                // show the error to the user
+                print(error)
+            } else if let token = token {
+                /*self.submitTokenToBackend(token, completion: { (error: NSError?) in
+                    if let error = error {
+                        // show the error to the user
+                    } else {
+                        // show a receipt page
+                        print("success")
+                    }
+                })*/
+                print("Got token! Token: \(token)")
+                self.postStripeToken(token)
+            }
+        }
     }
-
+    
+    func submitTokenToBackend(token: STPToken, completion: ( ( NSError? ) -> Void) ){
+        
+    }
+    
+    func postStripeToken(token: STPToken) {
+        
+        let URL = "http://localhost/donate/payment.php"
+        let params = ["stripeToken": token.tokenId]
+        print(params)
+        /*let params = ["stripeToken": token.tokenId,
+            "amount": self.amountTextField.text.toInt()!,
+            "currency": "usd",
+            "description": self.emailTextField.text]*/
+        
+        /*let manager = AFHTTPRequestOperationManager()
+        manager.POST(URL, parameters: params, success: { (operation, responseObject) -> Void in
+            
+            if let response = responseObject as? [String: String] {
+                UIAlertView(title: response["status"],
+                    message: response["message"],
+                    delegate: nil,
+                    cancelButtonTitle: "OK").show()
+            }
+            
+            }) { (operation, error) -> Void in
+                self.handleError(error!)
+        }*/
+    
+    }
+    
+    func handleError(error: NSError) {
+        let alert = UIAlertController(title: "Please Try Again", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
